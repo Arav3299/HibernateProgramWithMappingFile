d00b32f824df4eb52ceb78763bc713b3ccbc2ada
Arav3299 <101991714+Arav3299@users.noreply.github.com>
2022-05-12 05:43:35 -0230

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,25 @@
+package test;
+
+public class Test {
+	
+	public int testId;
+	public String testName;
+	
+public Test()
+{
+	       
+}
+public int getTestId() {
+	return testId;
+}
+public void setTestId(int testId) {
+	this.testId = testId;
+}
+public String getTestName() {
+	return testName;
+}
+public void setTestName(String testName) {
+	this.testName = testName;
+}
+
+}
